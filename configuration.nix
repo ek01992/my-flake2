201839f1648077eae3ae9854c211a77eb630a34e
Erik <ek01992@proton.me>
2026-04-15 13:43:05 -0500

    add console, timezone, and locale

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,15 @@
 
   networking = {
     hostId = "e1add2a5";
+    hostName = "nixxy";
     networkmanager.enable = true;
+  };
+
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
